04294c77c6cccb2ed9b9bc3ac39a5ec5ff1866fb
Sennysen <113637629+Sennysen@users.noreply.github.com>
2024-04-16 23:31:40 -0400

    hyperparameter ajustement

--- Perceptron.py
+++ Perceptron.py
@@ -61,26 +61,26 @@ print(classification_report(y_test, y_pred))
 
 
 
-classifier = MLPClassifier(hidden_layer_sizes=(500, 100), activation='relu', random_state=30)
+classifier = MLPClassifier(hidden_layer_sizes=(10, 5), activation='relu', random_state=30)
 X = vec_tfidf.fit_transform([' '.join(inner_list) for inner_list in cleanTweets])
 X_train, X_test, y_train, y_test = train_test_split(X, label, test_size=0.3, random_state=30)
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
-print("Accuracy of MLP with 2 layers: 500, 100 and relu as activation function with tokenization, stopword removal, stem and TFIDF:", accuracy_score(y_test, y_pred))
+print("Accuracy of MLP with 2 layers: 10, 5 and relu as activation function with tokenization, stopword removal, stem and TFIDF:", accuracy_score(y_test, y_pred))
 print("\nClassification Report:")
 print(classification_report(y_test, y_pred))
 
 
 
 
-classifier = MLPClassifier(hidden_layer_sizes=(30, 20), activation='logistic', random_state=30)
+classifier = MLPClassifier(hidden_layer_sizes=(10, 5), activation='logistic', random_state=30)
 X = vec_tfidf.fit_transform([' '.join(inner_list) for inner_list in cleanTweets])
 X_train, X_test, y_train, y_test = train_test_split(X, label, test_size=0.3, random_state=30)
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
-print("Accuracy of MLP with 2 layers: 30, 20 and logistic as activation function with tokenization, stopword removal, stem and TFIDF:", accuracy_score(y_test, y_pred))
+print("Accuracy of MLP with 2 layers: 10, 5 and logistic as activation function with tokenization, stopword removal, stem and TFIDF:", accuracy_score(y_test, y_pred))
 print("\nClassification Report:")
 print(classification_report(y_test, y_pred))
 
